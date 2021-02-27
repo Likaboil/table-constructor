3b9bd870dd52aa9c077ca368372296c595c71aa5
Liliya Sagitova <guverboil@gmail.com>
2021-02-27 19:18:34 +0300

    Добавляет конфигурацию приложения
    Удаляет файлы по умолчанию.
    Добавляет кастомный favicon.
    Добавляет глобальные стили.
    Добавляет шрифты.
    Рендер Table-screen

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.scss';
+
+const App = () => {
+  return (
+    <div>
+      <header className='app-header'>
+        <h1 className="title">Table-constructor</h1>
+      </header>
+    </div>
+
+  );
+};
+
+export default App;
